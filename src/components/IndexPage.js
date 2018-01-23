db0602bf233294f7221fe10320abdd0e0f7b3a19
ndillard1 <35346716+ndillard1@users.noreply.github.com>
2018-01-22 18:30:44 -0600

    hopefully back to normal for real this time

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -10,7 +10,7 @@ export const IndexPage = ({ students }) => (
         studentData => <StudentLink key={studentData.id} {...studentData} />,
       )}
     </div>
-      //  <h2><strong>Filter By Major</strong></h2>
+       <h2><strong>Filter By Major</strong></h2>
   </div>
 );
 
